b9b5813efbef5e1034aa3d8578bbae9cef007f52
Thomas <thomasmartin98@hotmail.com>
2021-05-31 22:10:18 +0100

    Messing around with some styles nothing impressive

--- FlaskAPP/app/routes.py
+++ FlaskAPP/app/routes.py
@@ -5,6 +5,7 @@ from flask import render_template
 def index():
      return render_template('index.html')
 
+#all I have to do is return the correct countries data, simple
 @app.route('/<path:country>')
 def retCountry(country):
      print(country)
