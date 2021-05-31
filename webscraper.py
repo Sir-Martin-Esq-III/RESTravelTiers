0fd92eb3d0b8967a26b7fd4e673264cae84207f0
Thomas Martin <ThomasMartin98@hotmail.com>
2021-05-31 20:33:48 +0100

    Updated webscraper
    Added functionality to add all of the countries and their tier to a JSON file with the correct format, also provided the JSON file in this commit

--- webscraper.py
+++ webscraper.py
@@ -1,33 +1,33 @@
 '''
 What this program does
-    It scrapes the UK govt website which is updated by them and fetches the tables relating to the 3
-    tiers "red" "amber" and "green" and creates JSON files which can be used by anyone who requests them
-
-What still needs doing
-    Finish designing the API format
-        format the lists in to the correct JSON notation
+    It scrapes the UK govt website which is updated by them and fetches the
+    tables relating to the 3 tiers "red" "amber" and "green" and creates a 
+    JSON file which can be used by anyone who requests them
 '''
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
 import re
 
 #Goes through table data to seperate the countries and adds them to a list
-def retCountries(countries):
-    countriesList=[]
+def retCountries(countries,ColorValue,jsonDict):
     tableBody=countries.find('tbody')
     for row in tableBody:
         val=re.findall(">(.*)<",str(row.find('th')))
-        #Empty lists would actually be (soon to be) updates to the list
-        if len(val)>0:
-            countriesList.append(val)    
-    return countriesList
+        try:                                            #Sometimes val[0]
+            if len(val[0])>0:                           #Cannot be accessed
+                jsonDict[val[0]]={"Tier":ColorValue}    #So we just pass these
+        except:                                         #Instances
+            pass
 
 #Gets webpage and parses the HTMl
-url="https://www.gov.uk/guidance/red-amber-and-green-list-rules-for-entering-england"
+url=("https://www.gov.uk/guidance/red-amber-and-green-list-rules-for-entering-england")
 page=urlopen(url)
 html =page.read().decode("utf-8")
 soup = BeautifulSoup(html,"html.parser")
+
+finalJsonObject={}
 
 #Seperates the 3 tables to its actual colors
 red,amber,green =soup.find_all("table")
@@ -36,9 +36,13 @@ colors= [red,amber,green]
 
 for color in colors:
     ColorValue=str(re.findall(">(.*)<",str(color.find('th')))[0]).split()[0]
-    print(ColorValue,'\t')
-    print(retCountries(color),'\n')
-    #Convert the lists in to valid JSON format (when I decide how to format the API)
+    retCountries(color,ColorValue,finalJsonObject)
+
+#Write to JSON file
+with open('data.json','w') as File:
+    json.dump(finalJsonObject,File)
+
+
 
 
 
